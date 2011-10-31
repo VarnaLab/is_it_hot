82fc9ef4a993a2b1bf9c542aa7ddea6840d00f7d
versi-hw <versi-hw@versi-hw-desktop.(none)>
2011-10-31 20:51:15 +0200

    parsing done

--- serial.js
+++ serial.js
@@ -8,4 +8,16 @@
   
   sp.on("data", function (data) {
     console.log("::"+data);
+    //console.log(data.split("DATA"));
+    var splitted1 = data.split("A:")
+
+    if(splitted1[1]!=undefined)
+      {
+            var splitted2 = splitted1[1].split(",T: ");
+            var temp = splitted2[1].split("\r");            
+var address = splitted2[0];
+                       
+console.log("DEBUG :: Extracted :: Address: "+address+", Temperature: "+temp);
+  
+      }
   });
